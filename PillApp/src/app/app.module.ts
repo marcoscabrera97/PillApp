c1ff9dc68a942ed3a9b017bb88d104e866cc5473
marcos.cabreram@e-campus.uab.cat <marcos.cabreram@e-campus.uab.cat>
2019-09-10 17:48:07 +0200

    Instalación bootstrap y navbar para probar

--- PillApp/src/app/app.module.ts
+++ PillApp/src/app/app.module.ts
@@ -4,10 +4,12 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
